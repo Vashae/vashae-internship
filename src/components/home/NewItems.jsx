09e78f71224497a9f2f11e09d8926c2841742b7c
Vashae <vashaeb2@gmail.com>
2024-12-16 03:46:38 -0500

    changed mapping as well as took extra slider out

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -98,7 +98,7 @@ const NewItems = () => {
       return () => clearInterval(interval);
     }, [expiryDate, timeLeft]);
   
-    const getAmericanFormattedTime = (milliseconds) => {
+    const getFormattedTime = (milliseconds) => {
       if (milliseconds <= 0) return "";
   
       const totalSeconds = Math.floor(milliseconds / 1000);
@@ -110,7 +110,7 @@ const NewItems = () => {
       return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`;
     };
   
-    return <div>{getAmericanFormattedTime(timeLeft)}</div>;
+    return <div>{getFormattedTime(timeLeft)}</div>;
   };
   
 
@@ -126,21 +126,20 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {isLoading ? (
-            <Slider {...settings}>
-              {Array(6)
-                .fill(0)
+          <Slider {...settings}>
+          {isLoading ? 
+           
+              Array.from({length:7})
                 .map((_, index) => (
                   <div key={index} className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
                     <Skeleton height={300} />
                     <Skeleton width="80%" style={{ margin: "10px 0" }} />
                     <Skeleton width="60%" />
                   </div>
-                ))}
-            </Slider>
-          ) : (
-            <Slider {...settings}>
-              {newItems.slice(0, 7).map((item) => (
+                ))
+           
+           : 
+              Array.isArray(newItems) && newItems.slice(0, 7).map((item) => (
                 <div
                   className="col-lg-3 col-md-6 col-sm-6 col-xs-12"
                   key={item.id}
@@ -156,29 +155,45 @@ const NewItems = () => {
                         <img className="lazy" src={item.authorImage} alt="" />
                         <i className="fa fa-check"></i>
                       </Link>
+                      
                     </div>
                     
                     <div className="de_countdown"> <Countdown expiryDate={item.expiryDate} /></div>
-                   
+                    
+
 
                     <div className="nft__item_wrap">
+                      
+                    
+                    
                     <div className="nft__item_extra">
+                    
                     <div className="nft__item_buttons">
+                    
                       <button>Buy Now</button>
+                      
                       <div className="nft__item_share">
+                      
                         <h4>Share</h4>
+                        
                         <a href="" target="_blank" rel="noreferrer">
+                      
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
+                        
                         <a href="" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
                         <a href="">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
+                        
                       </div>
+                      
                     </div>
+                    
                   </div>
+                  
                       <Link to="/item-details">
                         <img
                           src={item.nftImage}
@@ -187,6 +202,7 @@ const NewItems = () => {
                         />
                       </Link>
                     </div>
+                    
                     <div className="nft__item_info">
                       <Link to="/item-details">
                         <h4>{item.title}</h4>
@@ -195,13 +211,14 @@ const NewItems = () => {
                       <div className="nft__item_like">
                         <i className="fa fa-heart"></i>
                         <span>{item.likes}</span>
+                        </div>
+                        
                       </div>
                     </div>
-                  </div>
+                 
                   </div>
                  ))}
             </Slider>
-          )}
         </div>
       </div>
     </section>
