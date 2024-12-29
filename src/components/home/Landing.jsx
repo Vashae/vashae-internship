1422993600cc48cf9be19f89c1992b38c795eca2
Vashae <vashaeb2@gmail.com>
2024-12-28 19:03:11 -0500

    Hitting API, added skeleton loading states, as well as added home page animations

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -21,31 +21,54 @@ const Landing = () => {
             <div className="col-md-6">
               <div className="spacer-single"></div>
               <h6>
-                <div dat-aos='fade-in'
-                data-aos-delay='50'>
+                <div data-aos='fade-in'
+                data-aos-delay='500'
+                data-aos-offset='200'
+                data-aos-ease-in='ease-in-out'
+                data-aos-duration='1000'>
                 <span className="text-uppercase id-color-2">
                   Ultraverse Market
                 </span>
                 </div>
               </h6>
               <div className="spacer-10"></div>
-              <div dat-aos='fade-in'
-                data-aos-delay='50'>
+              <div data-aos='fade-up'
+                data-aos-delay='1000'
+                data-aos-ease-in='ease-in-out'
+                data-aos-duration='1200'>
               <h1>Create, sell or collect digital items.</h1>
               </div>
+              
+              <div data-aos='fade-up'
+                data-aos-delay='1000'
+                data-aos-ease-in='ease-in-out'
+                data-aos-duration='1500'>
               <p className="lead">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
                 interchangeable
               </p>
+              </div>
               <div className="spacer-10"></div>
+              <div data-aos='fade-in'
+              data-aos-delay='1200'
+              data-aos-ease-in='ease-in-out'
+              data-aos-duration='2000'>
               <Link className="btn-main lead" to="/explore">
+              
                 Explore
+               
               </Link>
+              </div>
               <div className="mb-sm-30"></div>
             </div>
             <div className="col-md-6 xs-hide">
+            <div data-aos='fade-in'
+              data-aos-delay='1200'
+              data-aos-ease-in='ease-in-out'
+              data-aos-duration='500'>
               <img src={NFT} className="lazy img-fluid" alt="" />
+              </div>
             </div>
           </div>
         </div>
